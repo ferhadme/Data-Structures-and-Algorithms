a8c47e4efee5a6b3b5217ebfbe8c60c6e90cce4d
ferhad2207 <frhdmehdiyev@gmail.com>
2020-09-22 16:56:04 +0400

    Function for checking if array is sorted or not is added

--- Algorithms/RecursionAndBacktracking/src/com/ferhad/Main.java
+++ Algorithms/RecursionAndBacktracking/src/com/ferhad/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         Recursion rc = new Recursion();
-        rc.towersOfHanoi(4, 'A', 'B', 'C');
+        // rc.towersOfHanoi(4, 'A', 'B', 'C');
+
+        // boolean isSorted = rc.isArrayInSortedOrder(new int[] {1, 2, 5, 3, 5, 6}, 0);
+        // System.out.println(isSorted);
+
 
     }
 }
--- Algorithms/RecursionAndBacktracking/src/com/ferhad/Recursion.java
+++ Algorithms/RecursionAndBacktracking/src/com/ferhad/Recursion.java
@@ -3,13 +3,13 @@ package com.ferhad;
 public class Recursion {
 
     /*
-    * Towers of Hanoi puzzle (for description of puzzle refer to https://en.wikipedia.org/wiki/Tower_of_Hanoi )
-    * Algorithm:
-    * Move the top n-1 disks from Source to Auxiliary tower;
-    * Move n^th disk from Source to Destination tower;
-    * Move the n-1 disks from Auxiliary tower to Destination tower;
-    * Transferring the top n-1 disks from Source to Auxiliary tower can again be thought of as a fresh problem and can
-    * be solved in the same manner.
+     * Towers of Hanoi puzzle (for description of puzzle refer to https://en.wikipedia.org/wiki/Tower_of_Hanoi )
+     * Algorithm:
+     * Move the top n-1 disks from Source to Auxiliary tower;
+     * Move n^th disk from Source to Destination tower;
+     * Move the n-1 disks from Auxiliary tower to Destination tower;
+     * Transferring the top n-1 disks from Source to Auxiliary tower can again be thought of as a fresh problem and can
+     * be solved in the same manner.
     */
     public void towersOfHanoi(int n, char fromRod, char toRod, char auxRod) {
         if (n == 1) {
@@ -24,5 +24,17 @@ public class Recursion {
 
         // Move n-1 disks from B to C using A as auxiliary
         towersOfHanoi(n - 1, auxRod, toRod, fromRod);
+    }
+
+    /* Function for checking if array is sorted or not
+     * We should give 0 value to index parameter while calling function. We can write also 2 different functions for one
+     * with @param arr, @param index, and the other one with only @param arr (one for calling, the other one for
+     * recursive calling, but we needn't it)
+    */
+    public boolean isArrayInSortedOrder(int[] arr, int index) {
+        if (index == arr.length - 1) {
+            return true;
+        }
+        return arr[index] <= arr[index + 1] && isArrayInSortedOrder(arr, index + 1);
     }
 }
