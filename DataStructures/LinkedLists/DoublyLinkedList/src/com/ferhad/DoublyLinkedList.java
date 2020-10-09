d0ba4ae7307b0506e7b3bf3991ae8701c722c62d
ferhad2207 <frhdmehdiyev@gmail.com>
2020-10-09 19:51:50 +0400

    DoublyLinkedList is created

---
+++ DataStructures/LinkedLists/DoublyLinkedList/src/com/ferhad/DoublyLinkedList.java
@@ -0,0 +1,18 @@
+package com.ferhad;
+
+/**
+ * @author Ferhad Mehdizade
+ * Implementation of Doubly LinkedList
+ * Insert O(n) (in case of inserting to beginning time complexity is O(1))
+ * Delete O(n) (in case of deleting from beginning time complexity is O(1))
+ * Access O(n)
+ * Search O(n)
+ * Good for insertion and deletion process as the Singly LinkedList. But the advantage of a doubly linked list (also
+        called two – way linked list) is that given a node in the list, we can navigate in both directions.
+ * But each node requires an extra pointer, requiring more space
+ * Insertion or deletion of a node takes a bit longer
+ */
+
+public class DoublyLinkedList {
+
+}
