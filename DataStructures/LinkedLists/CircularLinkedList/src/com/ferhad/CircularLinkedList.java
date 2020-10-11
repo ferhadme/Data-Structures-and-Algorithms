39b07744af280cefc093b25546f931dd4b41945f
ferhad2207 <frhdmehdiyev@gmail.com>
2020-10-11 22:28:49 +0400

    Circular LinkedList is created

---
+++ DataStructures/LinkedLists/CircularLinkedList/src/com/ferhad/CircularLinkedList.java
@@ -0,0 +1,17 @@
+package com.ferhad;
+
+/**
+ * @author Ferhad Mehdizade
+ * Implementation of Circular LinkedList
+ * Insert O(n) (in case of inserting to beginning time complexity is O(1))
+ * Delete O(n) (in case of deleting from beginning time complexity is O(1))
+ * Access O(n)
+ * Search O(n)
+ * Good for insertion and deletion process. A circular linked list is a variation of a linked list in which the last node
+ *      points to the first node, completing a full circle of nodes. In other words, this variation of the linked list
+ *      doesn't have a null element at the end.
+ */
+
+public class CircularLinkedList {
+    
+}
