243e2b077f172a0a05f294d3a7f9d5e5d924b605
ferhad2207 <frhdmehdiyev@gmail.com>
2020-10-21 20:31:18 +0400

    Files are modified

--- DataStructures/LinkedLists/CircularLinkedList/README.md
+++ DataStructures/LinkedLists/CircularLinkedList/README.md
@@ -1,2 +1,13 @@
 # Circular LinkedList
 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Circularly-linked-list.svg/525px-Circularly-linked-list.svg.png" />
+
+A circular linked list is a variation of a linked list in which the last node points to the first node, completing
+a full circle of nodes. In other words, this variation of the linked list doesn't have a null element at the end.
+
+Good for insertion, deletion process and this is very useful in the implementation of the queue data structure.
+
+Insert O(n) (in case of inserting to beginning time complexity is O(1))
+ * Delete O(n) (in case of deleting from beginning time complexity is O(1))
+ * Access O(n)
+ * Search O(n)
+
--- DataStructures/LinkedLists/CircularLinkedList/src/com/ferhad/CircularLinkedList.java
+++ DataStructures/LinkedLists/CircularLinkedList/src/com/ferhad/CircularLinkedList.java
@@ -2,14 +2,7 @@ package com.ferhad;
 
 /**
  * @author Ferhad Mehdizade
- * Implementation of Circular LinkedList
- * Insert O(n) (in case of inserting to beginning time complexity is O(1))
- * Delete O(n) (in case of deleting from beginning time complexity is O(1))
- * Access O(n)
- * Search O(n)
- * Good for insertion, deletion process and this is very useful in the implementation of the queue data structure.
- *      A circular linked list is a variation of a linked list in which the last node points to the first node, completing
- *      a full circle of nodes. In other words, this variation of the linked list doesn't have a null element at the end.
+ * Implementation of Circular LinkedList     
  */
 
 public class CircularLinkedList<T> {
