c1623d60f15f1ad49a899478bb09f165414c4410
ferhad2207 <frhdmehdiyev@gmail.com>
2020-11-01 14:45:54 +0400

    Brute Force solution is implemented for finding nth Node from end of the LinkedList

---
+++ DataStructures/LinkedLists/Problems/src/com/ferhad/Node.java
@@ -0,0 +1,13 @@
+package com.ferhad;
+
+/**
+ * This class is just for describing Nodes of LinkedList in the Problems.
+ */
+public class Node {
+    private Node next;
+
+    public Node getNext() {
+        return next;
+    }
+
+}
--- DataStructures/LinkedLists/Problems/src/com/ferhad/NthNodeFromEnd.java
+++ DataStructures/LinkedLists/Problems/src/com/ferhad/NthNodeFromEnd.java
@@ -1,4 +1,45 @@
 package com.ferhad;
 
+/**
+ * We can find Node from the end easily in the specified position if we know length of this LinkedList
+ * This class represents different methods with different approaches for finding specific Node from end if
+ * length is unknown
+ */
 public class NthNodeFromEnd {
+
+    /**
+     * This solution needs two scans: one for finding the length of the list and the other for
+     * finding (M – n + 1)th node from the beginning.
+     * @param head head of the LinkedList
+     * @param position position from the end
+     * @return Node in the specified position
+     * Time complexity = O(2n) for 2 scans => O(n)
+     * Space complexity = O(1)
+     */
+    public Node findNthNodeBruteForce(Node head, int position) {
+        Node temp = head;
+        int length = 0;
+
+        if (temp != null) {
+            length++;
+            while (temp.getNext() != null) {
+                length++;
+                temp = temp.getNext();
+            }
+        } else {
+            return null;
+        }
+
+        int positionFromBeginning = length - position - 1; // 1 for length
+        if (positionFromBeginning < 0)
+            throw new IndexOutOfBoundsException();
+        temp = head;
+
+        for (int i = 0; i < positionFromBeginning; i++) {
+            temp = temp.getNext();
+        }
+        return temp;
+    }
+
+
 }
