49609629909566629e1c5c4c55a0d837f3c0365b
ferhad2207 <frhdmehdiyev@gmail.com>
2020-11-02 20:35:10 +0400

    Method for finding Intersected Node of 2 Singly LinkedLists is implemented by Stack approach

--- DataStructures/LinkedLists/Problems/src/com/ferhad/problems/IntersectedTwoSinglyLinkedLists.java
+++ DataStructures/LinkedLists/Problems/src/com/ferhad/problems/IntersectedTwoSinglyLinkedLists.java
@@ -3,6 +3,7 @@ package com.ferhad.problems;
 import com.ferhad.Node;
 
 import java.util.Hashtable;
+import java.util.Stack;
 
 /**
  * @author Ferhad Mehdizade
@@ -46,21 +47,21 @@ public class IntersectedTwoSinglyLinkedLists {
      * @param head1 head of the first Singly LinkedList
      * @param head2 head of the second Singly LinkedList
      * @return intersected Node
-     * Time complexity - O(n) + O(m), where n for creating HashTable, and m for scanning the second list
+     * Time complexity - O(n + m), where n for creating HashTable, and m for scanning the second list
      * Space complexity - O(n) for creating HashTable
      */
     public Node findIntersectedNodeHT(Node head1, Node head2) {
-        Hashtable<Node, Integer> nodeRecords = new Hashtable<>();
+        Hashtable<Node, Integer> nodeAddresses = new Hashtable<>();
         int i = 0; // just for counter
         while (head1 != null) {
-            nodeRecords.put(head1, i);
+            nodeAddresses.put(head1, i);
             i++;
             head1 = head1.getNext();
         }
 
         while (head2 != null) {
-            if (nodeRecords.containsKey(head2)) {
-                i = nodeRecords.get(head2); // intersection is happen in i(th) Node
+            if (nodeAddresses.containsKey(head2)) {
+                i = nodeAddresses.get(head2); // intersection is happen in i(th) Node
                 return head2;
             }
             head2 = head2.getNext();
@@ -68,4 +69,36 @@ public class IntersectedTwoSinglyLinkedLists {
         // there is no intersected Node
         return null;
     }
+
+    /**
+     * Finds intersected Node using Stacks(S indicates Stack)
+     * @param head1 head of the first Singly LinkedList
+     * @param head2 head of the second Singly LinkedList
+     * @return intersected Node
+     * Time complexity - O(m + n) for scanning 2 times
+     * Space complexity - O(m + n) for creating 2 Stacks
+     */
+    public Node findIntersectedNodeS(Node head1, Node head2) {
+        Stack<Node> nodeAddresses1 = new Stack<>();
+        Stack<Node> nodeAddresses2 = new Stack<>();
+        int counter = 0;
+        while (head1 != null && head2 != null) {
+            nodeAddresses1.push(head1);
+            nodeAddresses2.push(head2);
+            head1 = head1.getNext();
+            head2 = head2.getNext();
+            counter++;
+        }
+
+        for (int i = 0; i < counter; i++) {
+            if (nodeAddresses1.peek().equals(nodeAddresses2.peek()))
+                return nodeAddresses1.peek();
+            nodeAddresses1.pop();
+            nodeAddresses2.pop();
+        }
+        // there is no intersected Node
+        return null;
+    }
+
+
 }
