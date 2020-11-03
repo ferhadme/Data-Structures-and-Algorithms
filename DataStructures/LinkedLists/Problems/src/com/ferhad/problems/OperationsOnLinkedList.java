aef05057646d4db9f0fe322a86803e42049e634e
ferhad2207 <frhdmehdiyev@gmail.com>
2020-11-03 21:43:38 +0400

    Iterative version of last method is implemented

--- DataStructures/LinkedLists/Problems/src/com/ferhad/problems/OperationsOnLinkedList.java
+++ DataStructures/LinkedLists/Problems/src/com/ferhad/problems/OperationsOnLinkedList.java
@@ -93,12 +93,15 @@ public class OperationsOnLinkedList {
     }
 
     /**
-     * Merging 2 LinkedLists into third LinkedList in sorted order
+     * Merging 2 LinkedLists into third LinkedList in sorted order recursively
      * @param head1 head of the first LinkedList
      * @param head2 head of the second LinkedList
      * @return head of the Merged LinkedList
+     * Time complexity - O(n + m)
+     * Space complexity - O(n + m) for recursive stack,
+     * where n and m are sizes respectively
      */
-    public Node merge2Lists(Node head1, Node head2) {
+    public Node merge2ListsR(Node head1, Node head2) {
         if (head1 == null)
             return head2;
         if (head2 == null)
@@ -106,10 +109,34 @@ public class OperationsOnLinkedList {
         Node head = new Node();
         if (head1.getData() >= head2.getData()) {
             head = head2;
-            head.setNext(merge2Lists(head2.getNext(), head1));
+            head.setNext(merge2ListsR(head2.getNext(), head1));
         } else {
             head = head1;
-            head.setNext(merge2Lists(head1.getNext(), head2));
+            head.setNext(merge2ListsR(head1.getNext(), head2));
+        }
+        return head;
+    }
+
+    /**
+     * Merging 2 LinkedLists into third LinkedList in sorted order iteratively
+     * @param head1 head of the first LinkedList
+     * @param head2 head of the second LinkedList
+     * @return head of the Merged LinkedList
+     * Time complexity - O(n + m), where n and m are sizes respectively
+     * Space complexity - O(1)
+     */
+    public Node merge2ListsI(Node head1, Node head2) {
+        Node head = new Node();
+        Node current = head;
+        while (head1 != null && head2 != null) {
+            if (head1.getData() >= head2.getData()) {
+                current.setNext(head2);
+                head2 = head2.getNext();
+            } else {
+                current.setNext(head1);
+                head1 = head1.getNext();
+            }
+            current = current.getNext();
         }
         return head;
     }
