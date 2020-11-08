10a498e7186b91a7b0c13b9dfba46a667c45e770
ferhad2207 <frhdmehdiyev@gmail.com>
2020-11-08 14:15:32 +0400

    Linked Stack is implemented

--- DataStructures/Stacks/LinkedStack/src/com/ferhad/Main.java
+++ DataStructures/Stacks/LinkedStack/src/com/ferhad/Main.java
@@ -3,6 +3,20 @@ package com.ferhad;
 public class Main {
 
     public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(3);
+        stack.push(2);
+        stack.push(65);
+        stack.pop();
+        System.out.println(stack.peek());
+        System.out.println(stack);
+        System.out.println(stack.contains(65));
+        System.out.println(stack.size());
+
+        stack.pop();
+        stack.pop();
+        System.out.println(stack);
+        System.out.println(stack.isEmpty());
 
     }
 }
---
+++ DataStructures/Stacks/LinkedStack/src/com/ferhad/Stack.java
@@ -0,0 +1,123 @@
+package com.ferhad;
+
+import java.util.EmptyStackException;
+
+/**
+ * @author Ferhad Mehdizade
+ *
+ * LinkedList Implementation of Stack Data Structure
+ * @param <E> Type of element
+ */
+public class Stack<E> {
+    private Element top; // top of the Stack
+    private int size;
+
+    /**
+     * Initializes size with 0
+     */
+    public Stack() {
+        size = 0;
+    }
+
+    /**
+     * Checks emptiness of the Stack
+     * @return true if Stack is empty, otherwise false
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Checks if Stack contains specified element or not
+     * @param e specified element
+     * @return true if Stack contains the element, otherwise false
+     */
+    public boolean contains(E e) {
+        for (Element temp = top; temp != null; temp = temp.next) {
+            if (temp.data.equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @return size of the Stack
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Pushes new element to top of the Stack
+     * @param e inserted element
+     */
+    public void push(E e) {
+        if (top == null) {
+            top = new Element(e);
+        } else {
+            Element element = new Element(e);
+            Element temp = top;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = element;
+        }
+        size++;
+    }
+
+    /**
+     * Removes element from top of the Stack
+     * @return popped element
+     */
+    public E pop() {
+        if (isEmpty())
+            throw new EmptyStackException();
+        Element poppedElement = top;
+        Element newTop = top.next;
+        top.next = null;
+        top = newTop;
+        size--;
+        return poppedElement.data;
+    }
+
+    /**
+     * Gets top element of the Stack
+     * @return top
+     */
+    public E peek() {
+        if (isEmpty())
+            throw new EmptyStackException();
+        return top.data;
+    }
+
+    /**
+     * Private class of Stack for linking elements of Stack each other
+     */
+    private class Element {
+        E data;
+        Element next;
+        Element(E data) {
+            this.data = data;
+        }
+    }
+
+    /**
+     * @return string representation of Stack as ["topElement", ..., "bottomElement"]
+     */
+    @Override
+    public String toString() {
+        String stackStr = "[";
+        Element temp = top;
+        while (temp != null) {
+            if (temp.next == null) {
+                stackStr += temp.data;
+            } else {
+                stackStr += temp.data + ", ";
+            }
+            temp = temp.next;
+        }
+        return stackStr + "]";
+    }
+
+}
