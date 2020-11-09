0d37a0d3ec49357144a6cf1423e1f12e7bdd4e48
ferhad2207 <frhdmehdiyev@gmail.com>
2020-11-09 15:38:56 +0400

    Checking patterns is valid or invalid is implemented

--- DataStructures/Stacks/Problems/src/com/ferhad/BalanceOfSymbols.java
+++ DataStructures/Stacks/Problems/src/com/ferhad/BalanceOfSymbols.java
@@ -1,5 +1,7 @@
 package com.ferhad;
 
+import java.util.Stack;
+
 /**
  * @author Ferhad Mehdizade
  *
@@ -10,5 +12,58 @@ package com.ferhad;
  * if (isEmpty() { System.out.println("It is empty); }      ==>     Invalid pattern(compiler gives error)
  */
 public class BalanceOfSymbols {
+
+    /**
+     * Checks if pattern is valid or not
+     * @param pattern input
+     * @return true if pattern is valid, otherwise false
+     * Time complexity - O(n) for scanning each character of the input
+     * Space complexity - O(n) for Stack
+     */
+    public boolean isValidSymbolPattern(String pattern) {
+        if (pattern == null || pattern.length() == 0)
+            return true;
+        Stack<Character> stack = new Stack<>();
+        boolean checker = false; // false when character like ", ' opened, true when closed
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char character = pattern.charAt(i);
+            if (character != '{' && character != '}' && character != '[' && character != ']' &&
+                    character != '(' && character != ')' && character != '\'' && character != '\"') {
+                continue;
+            }
+
+            if ((character == '\'' || character == '\"') && !checker) {
+                stack.push(character);
+                checker = true; // character is pushed
+            }
+            else if (character == '}') {
+                if (!stack.isEmpty() && stack.peek() == '{')
+                    stack.pop();
+                else return false;
+            }
+            else if (character == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(')
+                    stack.pop();
+                else return false;
+            }
+            else if (character == ']') {
+                if (!stack.isEmpty() && stack.peek() == '[')
+                    stack.pop();
+                else return false;
+            }
+            else if (character == '\'') {
+                if (!stack.isEmpty() && stack.peek() == '\'') {
+                    stack.pop();
+                    checker = false;
+                }
+                else return false;
+            }
+            else {
+                stack.push(character);
+            }
+        }
+        return stack.isEmpty();
+    }
 
 }
--- DataStructures/Stacks/Problems/src/com/ferhad/Main.java
+++ DataStructures/Stacks/Problems/src/com/ferhad/Main.java
@@ -3,6 +3,10 @@ package com.ferhad;
 public class Main {
 
     public static void main(String[] args) {
+        BalanceOfSymbols bos = new BalanceOfSymbols();
+        String pattern = "{[a('uuiui')sa''{asas''''}]}"; // true pattern
+        System.out.println(bos.isValidSymbolPattern(pattern));
+
 
     }
 }
