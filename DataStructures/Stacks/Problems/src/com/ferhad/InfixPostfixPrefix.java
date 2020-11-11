8f1375dc8fa65c2901190570d8bb171858db98c9
ferhad2207 <frhdmehdiyev@gmail.com>
2020-11-11 17:36:04 +0400

    Infix to Postfix conversion algorithm is described

--- DataStructures/Stacks/Problems/src/com/ferhad/InfixPostfixPrefix.java
+++ DataStructures/Stacks/Problems/src/com/ferhad/InfixPostfixPrefix.java
@@ -1,7 +1,31 @@
 package com.ferhad;
 
 /**
+ * @author Ferhad Mehdizade
  *
+ * Infix to Postfix conversion algorithm using Stack Data Structure
+ * Computers calculate mathematical expressions in right order using these algorithms
+ * Basic version is described in here
+ *
+ * Firstly, what is infix, prefix, postfix?
+ * Infix ==>     (A + B) + (C - D)       ==> We input and understand mathematical expressions in this form
+ *
+ * But Computers, use prefix and postfix in order to understanding mathematical expressions
+ *
+ * => (A + B) + (C - D)
+ * => +AB(sum of A and B)
+ * => ++AB(adding ... to sum of A and B)
+ * => -CD(... = subtract D from C)
+ * => Finally, Prefix is ==>    ++AB-CD
+ *
+ * Postfix is easier for conversion. That's why most system uses Postfix
+ * => (A + B) + (C - D)
+ * => AB+
+ * => CD-
+ * => Postfix =>    AB+CD-+ (Sum of, A + B and C - D)
+ *
+ * I use Postfix for conversion and calculating
  */
 public class InfixPostfixPrefix {
+    
 }
