b4b49f76f0fce4ece5ac19074246fbc90b821d9f
ferhad2207 <frhdmehdiyev@gmail.com>
2020-11-18 19:19:52 +0400

    SpecificStack code is modified

--- DataStructures/Stacks/Problems/src/com/ferhad/Main.java
+++ DataStructures/Stacks/Problems/src/com/ferhad/Main.java
@@ -16,14 +16,13 @@ public class Main {
 */
         // =============================================================================================================
 /*
-        var stack = new SpecificStack<Integer>();
+        var stack = new SpecificStack();
         stack.push(1);
         stack.push(2);
         stack.push(6);
         stack.push(5);
         stack.push(3);
         stack.push(4);
-        System.out.println(stack.getMaximum());
         System.out.println(stack.getMinimum());
 */
         // =============================================================================================================
--- DataStructures/Stacks/Problems/src/com/ferhad/SpecificStack.java
+++ DataStructures/Stacks/Problems/src/com/ferhad/SpecificStack.java
@@ -1,35 +1,60 @@
 package com.ferhad;
 
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 /**
  * @author Ferhad Mehdizade
  *
- * Designing a stack such that getMinimum( ) and getMaximum( ) should be O(1)
- * @param <E> Type of an element
+ * Designing a stack such that getMinimum( ) should be O(1)
  */
-public class SpecificStack<E extends Comparable<E>> extends Stack<E> {
-    private E min;
-    private E max;
+public class SpecificStack {
+    private Stack<Integer> s = new Stack<>(); // main stack to store minimum element
+    private int min; // minimum element of the Stack
 
-    @Override
-    public E push(E item) {
-        if (isEmpty()) {
-            min = item;
-            max = item;
-        } else if (item.compareTo(max) >= 0)
-            max = item;
-        else if (item.compareTo(min) <= 0)
-            min = item;
-        return super.push(item);
+    /**
+     * Pushes data to the Stack
+     * @param data inserted data
+     */
+    public void push(int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            min = data;
+        } else if (data > min) {
+            s.push(data);
+        } else {
+            s.push(data * 2 - min);
+            min = data;
+        }
     }
 
-    public E getMinimum() {
+    /**
+     * Removes top element from the Stack
+     * @param data top data
+     */
+    public void pop(int data) {
+        if (s.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        int top = s.peek();
+        if (top < min) {
+            min = 2 * min - top;
+        }
+        s.pop();
+    }
+
+    /**
+     * @return minimum element of the Stack
+     */
+    public int getMinimum() {
         return min;
     }
 
-    public E getMaximum() {
-        return max;
+    /**
+     * @return String representation of the Main Stack
+     */
+    @Override
+    public String toString() {
+        return s.toString();
     }
-
 }
