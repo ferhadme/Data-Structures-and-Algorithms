ead87558767467e1847b5dbba18afe77422cc327
ferhad2207 <frhdmehdiyev@gmail.com>
2020-12-02 22:47:34 +0400

    Fixed size Queue is implemented with Circular Array

--- DataStructures/Queues/FixedQueue/src/com/ferhad/Main.java
+++ DataStructures/Queues/FixedQueue/src/com/ferhad/Main.java
@@ -3,6 +3,26 @@ package com.ferhad;
 public class Main {
 
     public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<>();
+        queue.enQueue(2);
+        queue.enQueue(3);
+        queue.enQueue(4);
+        queue.enQueue(87);
+        queue.enQueue(12);
+        queue.enQueue(23);
+        // [2, 3, 4, 87, 12, 23]
 
+        queue.deQueue(); // 2
+        queue.deQueue(); // 3
+        // [4, 87, 12, 23]
+
+        System.out.println(queue.getFront()); // 4
+        System.out.println(queue.getRear()); // 23
+        System.out.println(queue);
+
+        System.out.println(queue.isEmpty()); // false
+        System.out.println(queue.isFull()); // false
+
+        System.out.println(queue.size()); // 4
     }
 }
---
+++ DataStructures/Queues/FixedQueue/src/com/ferhad/Queue.java
@@ -0,0 +1,126 @@
+package com.ferhad;
+
+import java.util.NoSuchElementException;
+
+/**
+ * @author Ferhad Mehdizade
+ *
+ * Implementation of Queue Data Structure using Circular Array
+ * @param <T> Type of an element
+ */
+public class Queue<T> {
+    private static final int CAPACITY;
+    static {
+        CAPACITY = 16; // default size for fixed size Queue
+    }
+    private Object[] elements; // elements of Queue
+    private int size; // size of Queue
+    private int front; // front of Queue
+    private int rear; // rear of Queue
+    private int capacity; // optional capacity
+
+    /**
+     * Initializes capacity with default capacity(16)
+     */
+    public Queue() {
+        elements = new Object[CAPACITY];
+        capacity = CAPACITY;
+    }
+
+    /**
+     * Initializes capacity with optional capacity
+     * @param capacity optional capacity
+     */
+    public Queue(int capacity) {
+        elements = new Object[capacity];
+        this.capacity = capacity;
+    }
+
+    /**
+     * Gets elements by casting Object element to specific type
+     * @param index index of an element in actual array
+     * @return element with specific type
+     */
+    private T getElement(int index) {
+        return (T) elements[index];
+    }
+
+    /**
+     * Enqueues new element to the Queue
+     * @param elem inserted element
+     */
+    public void enQueue(T elem) {
+        if (size == capacity)
+            throw new IllegalStateException("Queue is full");
+        elements[rear++] = elem;
+        rear %= capacity; // for providing circular array logic
+        size++;
+    }
+
+    /**
+     * Dequeues element from Queue
+     * @return removed element from Queue
+     */
+    public T deQueue() {
+        if (size == 0)
+            throw new NoSuchElementException("Queue is empty");
+        T data = getElement(front % capacity);
+        elements[front++] = null;
+        front %= capacity;
+        size--;
+        return data;
+    }
+
+    /**
+     * Gets front(or first) element of the Queue
+     * @return front element
+     */
+    public T getFront() {
+        return getElement(front);
+    }
+
+    /**
+     * Gets rear(or last) element of the Queue
+     * @return rear element
+     */
+    public T getRear() {
+        return getElement(rear - 1);
+    }
+
+    /**
+     * @return size of the Queue
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Defines emptiness of Queue
+     * @return true if Queue is empty, otherwise false
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Defines fullness of Queue
+     * @return true if Queue is full, otherwise false
+     */
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    /**
+     * @return String representation of the Queue in the form [FirstElement, ..., LastElement]
+     */
+    @Override
+    public String toString() {
+        String queueRep = "[";
+        int i; // needed for the last element
+        for (i = 0; i < size - 1; i++) {
+            queueRep += elements[(front + i) % capacity] + ", ";
+        }
+        queueRep += elements[(front + i) % capacity] + "]";
+        return queueRep;
+    }
+}
