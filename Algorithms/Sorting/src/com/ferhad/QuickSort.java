0279b56ef46a9925fd8abc38c4232414ea827481
ferhad2207 <frhdmehdiyev@gmail.com>
2020-12-20 16:56:38 +0400

    Quick Sort Algorithm is implemented

--- Algorithms/Sorting/src/com/ferhad/QuickSort.java
+++ Algorithms/Sorting/src/com/ferhad/QuickSort.java
@@ -7,10 +7,21 @@ package com.ferhad;
  */
 public class QuickSort {
 
+    /**
+     * Function calls recursive quick sort
+     * @param arr array
+     */
     public static void quickSort(int[] arr) {
         quickSort(arr, 0, arr.length - 1);
     }
 
+    /**
+     * Defines partition index in each partition(...) call. Repeat the same process, for left and right part of index.
+     * Sorting happens in partition(...) function.
+     * @param arr array
+     * @param begin begin index of the sub array
+     * @param end end index of the sub array
+     */
     private static void quickSort(int[] arr, int begin, int end) {
         if (begin < end) {
             int partition = partition(arr, begin, end);
@@ -20,6 +31,15 @@ public class QuickSort {
         }
     }
 
+    /**
+     * Defines pivot in the array between begin and end indices. I take last element as pivot for simplicity
+     * Then basically, takes smaller elements than pivot to the left side, and larger elements to the right side
+     * @param arr array
+     * @param begin begin index of the sub array
+     * @param end end index of the sub array
+     * @return new partition index which indicates this element is in the correct place, do the same process for left
+     * and right side
+     */
     private static int partition(int[] arr, int begin, int end) {
         int pivot = arr[end];
         int i = begin - 1;
