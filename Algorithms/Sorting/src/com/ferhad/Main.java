b2b102c33de7de8dcec874ed93d253396d555d49
ferhad2207 <frhdmehdiyev@gmail.com>
2020-12-20 19:41:25 +0400

    Repo is updated

--- Algorithms/Sorting/src/com/ferhad/Main.java
+++ Algorithms/Sorting/src/com/ferhad/Main.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
+// TESTING...
 public class Main {
 
     public static void main(String[] args) {
-        // Testing...
         Scanner scan = new Scanner(System.in);
         int size = scan.nextInt();
         int[] arr = new int[size];
