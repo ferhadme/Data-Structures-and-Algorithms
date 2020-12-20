e83ce81c9b5587bc57ae8992fa6b8d281d817b40
ferhad2207 <frhdmehdiyev@gmail.com>
2020-12-20 08:56:37 +0400

    Bubble Sort Algorithm is implemented

---
+++ Algorithms/Sorting/src/com/ferhad/BubbleSort.java
@@ -0,0 +1,20 @@
+package com.ferhad;
+
+/**
+ * @author Ferhad Mehdizade
+ *
+ * Bubble Sort Algorithm
+ */
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
--- Algorithms/Sorting/src/com/ferhad/Main.java
+++ Algorithms/Sorting/src/com/ferhad/Main.java
@@ -1,23 +1,48 @@
 package com.ferhad;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        // Testing...
         Scanner scan = new Scanner(System.in);
         int size = scan.nextInt();
         int[] arr = new int[size];
         for (int i = 0; i < size; i++)
             arr[i] = scan.nextInt();
 
+        shuffle(arr);
+
         MergeSort.mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        shuffle(arr);
+
+        BubbleSort.bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
 
 
 
 
+    }
 
+    /**
+     * Shuffles elements of the array.
+     * @param arr array
+     */
+    private static void shuffle(int[] arr) {
+        Random rand = new Random();
+        for (int i = 0; i < 100; i++) {
+            int index1 = rand.nextInt(arr.length);
+            int index2 = rand.nextInt(arr.length);
+            int temp = arr[index1];
+            arr[index1] = arr[index2];
+            arr[index2] = temp;
+        }
     }
 }
+
+
