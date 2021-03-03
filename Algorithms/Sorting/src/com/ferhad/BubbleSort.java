96b2bdf23bada75995b48c8c2c0fc4e10ca95973
ferhad2207 <frhdmehdiyev@gmail.com>
2021-03-03 19:02:43 +0400

    Bit Manipulation is added

--- Algorithms/Sorting/src/com/ferhad/BubbleSort.java
+++ Algorithms/Sorting/src/com/ferhad/BubbleSort.java
@@ -12,15 +12,16 @@ public class BubbleSort {
      * @param arr array
      */
     public static void bubbleSort(int[] arr) {
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[i]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+        int n = arr.length;
+        while (n > 1) {
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
+            n--;
         }
     }
 
