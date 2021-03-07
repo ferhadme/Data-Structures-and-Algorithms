a1a682aa4846c45cd522d370bcb69e6305181fd8
ferhad2207 <frhdmehdiyev@gmail.com>
2021-03-07 22:27:54 +0400

    Bit Addition Algorithm is improved

--- Algorithms/BitManipulation/src/com/ferhad/Addition.java
+++ Algorithms/BitManipulation/src/com/ferhad/Addition.java
@@ -8,33 +8,29 @@ package com.ferhad;
 public class Addition {
 
     /**
-     *
+     * Adds 2 String of bits
      * @param bitString1 first bit string
      * @param bitString2 second bit string
      * @return sum of 2 bits in the string form
      */
     public static String add(String bitString1, String bitString2) {
-        bitString1 = bitStringFilter(bitString1);
-        bitString2 = bitStringFilter(bitString2);
-        StringBuilder sumSequence = new StringBuilder();
-        String minBitSequence = bitString1.length() <= bitString2.length() ? bitString1 : bitString2;
-        String maxBitSequence = bitString1.length() > bitString2.length() ? bitString1 : bitString2;
+        StringBuilder sum = new StringBuilder();
         int carry = 0;
-        int maxBitSeqIndex = maxBitSequence.length() - 1;
-        for (int i = minBitSequence.length() - 1; i >= 0; i--, maxBitSeqIndex--) {
-            int bitSum = addBits(Character.getNumericValue(minBitSequence.charAt(i)),
-                    Character.getNumericValue(maxBitSequence.charAt(maxBitSeqIndex)), carry);
-            sumSequence.append(bitSum % 10);
+        int i = bitString1.length() - 1;
+        int j = bitString2.length() - 1;
+        while (i >= 0 || j >= 0) {
+            int bitSum = addBits(
+                    (i >= 0) ? Character.getNumericValue(bitString1.charAt(i)) : 0,
+                    (j >= 0) ? Character.getNumericValue(bitString2.charAt(j)) : 0,
+                    carry
+            );
+            sum.insert(0, bitSum % 10);
             carry = bitSum / 10;
+            i--;
+            j--;
         }
-        while (maxBitSeqIndex >= 0) {
-            int bitSum = addBits(Character.getNumericValue(maxBitSequence.charAt(maxBitSeqIndex)), carry, 0);
-            sumSequence.append(bitSum % 10);
-            carry = bitSum / 10;
-            maxBitSeqIndex--;
-        }
-        sumSequence.append(carry);
-        return bitStringFilter(sumSequence.reverse().toString());
+        sum.insert(0, carry);
+        return bitStringFilter(sum.toString());
     }
 
     /**
@@ -42,7 +38,7 @@ public class Addition {
      * @param bit1 first bit
      * @param bit2 second bit
      * @param carry carry
-     * @return sum of bits and carry
+     * @return sum of bits and carry such as - sum = sum % 10 and carry = sum / 10
      */
     private static int addBits(int bit1, int bit2, int carry) {
         int sum = bit1 + bit2 + carry;
