86fa6ab35fdf95ac238692bf9d066d1e12675b78
Ferhad Mehdizade <frhdmehdiyev@gmail.com>
2025-10-05 02:06:00 +0400

    bit manipulation

--- Algorithms/BitManipulation/src/com/ferhad/techniques.c
+++ Algorithms/BitManipulation/src/com/ferhad/techniques.c
@@ -44,8 +44,42 @@ void togglebit(int *n, int i)
     *n ^= (1 << i);
 }
 
+/*
+ * Checks if number is power of two
+ */
 int is_power_of_two(int n)
 {
     return (n >= 0) && (n & (n - 1)) == 0;
 }
 
+/*
+ * Reverses number's bit representation
+ */
+int reverseBits(int n) {
+    int k = 32;
+    int res = 0;
+
+    while (n > 0) {
+	int last = n & 1;
+	res |= (last << --k);
+	n >>= 1;
+    }
+
+    return res;
+}
+
+/*
+ * Finds missing number in nums (len: n) in interval of 0..n
+ */
+int missingNumber(int* nums, int len) {
+    int res = 0;
+    for (int i = 1; i <= len; i++) {
+	res ^= i;
+    }
+
+    for (int i = 0; i < len; i++) {
+	res ^= nums[i];
+    }
+
+    return res;
+}
